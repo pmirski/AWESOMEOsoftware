fc45bab82a870d40980b36084e091c8403e4a962
Jake Wickstrom <jtwickstrom@alumni.ubc.ca>
2017-07-27 12:19:25 -0700

    IR Sensor Test Changes

--- TestCode/TestEdgeFollowing/TestEdgeFollowing/TestEdgeFollowing.ino
+++ TestCode/TestEdgeFollowing/TestEdgeFollowing/TestEdgeFollowing.ino
@@ -19,13 +19,18 @@ int motor_rightWheel = 1;
 int WHITE_SURFACE = 50;
 int MID_POINT = 400;
 int OVER_EDGE = 950;
+int kP = 10;         //proportional gain factor
 int M_CORRECT = 0; // medium correction
 int H_CORRECT = 0; // hard corretion
 
 void loop() {
+
+  edge_sensor_left = analogRead(1);
+  edge_sensor_right = analogRead(2);
+  
   timer++;
 
-  if(timer %300 == 0){
+  if(timer % 300 == 0){
     
     LCD.clear();
     LCD.home();
@@ -34,25 +39,28 @@ void loop() {
     LCD.print(edge_sensor_right);
   }
 
-  //FOR AN EDGE SENSOR ON THE FRONT RIGHT CORNER OF THE ROBOT
-  //High sensor readings correcct to the left; low sensor readings correct to the right
-  //Positive error = left correction; Negative error = right correction
+  // lower readings result in a negative correction (to the right) while high readings have a positive correction (to the left)
+  error = (edge_sensor_right - MID_POINT)/1023*kP;
 
-  //medium corrections
-  if(edge_sensor_right > WHITE_SURFACE && edge_sensor_right <= MID_POINT){
-    error = -1*M_CORRECT;
-  }
-  if(edge_sensor_right > MID_POINT && edge_sensor_right < OVER_EDGE){
-    error = M_CORRECT;
-  }
-
-  //hard corrections
-  if(edge_sensor_right <= WHITE_SURFACE){
-    error = -1*H_CORRECT;
-  }
-  if(edge_sensor_right >= OVER_EDGE){  
-    error = H_CORRECT;
-  }
+//  //FOR AN EDGE SENSOR ON THE FRONT RIGHT CORNER OF THE ROBOT
+//  //High sensor readings correcct to the left; low sensor readings correct to the right
+//  //Positive error = left correction; Negative error = right correction
+//
+//  //medium corrections
+//  if(edge_sensor_right > WHITE_SURFACE && edge_sensor_right <= MID_POINT){
+//    error = -1*M_CORRECT;
+//  }
+//  if(edge_sensor_right > MID_POINT && edge_sensor_right < OVER_EDGE){
+//    error = M_CORRECT;
+//  }
+//
+//  //hard corrections
+//  if(edge_sensor_right <= WHITE_SURFACE){
+//    error = -1*H_CORRECT;
+//  }
+//  if(edge_sensor_right >= OVER_EDGE){  
+//    error = H_CORRECT;
+//  }
   
   motor.speed(motor_leftWheel, SPEED - error);
   motor.speed(motor_rightWheel, SPEED + error);
---
+++ TestCode/TestIRSensor/TestIRSensor.ino
@@ -0,0 +1,20 @@
+#include <phys253.h>          
+#include <LiquidCrystal.h>
+
+void setup() {
+  #include <phys253setup.txt>
+  pinMode(A0,INPUT);
+}
+
+void loop() {
+  delay(30);
+  LCD.clear();
+  LCD.home();
+
+  if(digitalRead(A0) == HIGH){
+    LCD.print("Driving");
+  }else{
+    LCD.print("Stopped");
+  }
+
+}
